8826bffdfd1c062283ae8607dac1cd6c5f0f239f
hugojing <hugojingcn@gmail.com>
2015-10-10 15:11:36 +0800

    Components + Render + Data Model

--- app/component.jsx
+++ app/component.jsx
@@ -1,7 +1,81 @@
 import React from 'react';
 
-export default class Hello extends React.Component {
+var style = {
+	backgroundColor: '#EEE',
+	textAlign: 'center'
+};
+
+class Hello extends React.Component {
 	render() {
-		return <h1>ES6 + React + Webpack 示例</h1>;
+		return (
+			<div style={style}>
+			<br />
+			<h1>ES6 + React + Webpack 示例</h1>
+			<br />
+			</div>
+		)
 	}
+}
+
+class CommentBox extends React.Component {
+	render() {
+		return (
+			<div className='commentBox'>
+				<h1>Comments</h1>
+				<CommentList data = {this.props.data} />
+				<CommentForm />
+			</div>
+		)
+	}
+}
+
+
+
+class CommentList extends React.Component {
+	render() {
+		var commentNodes = this.props.data.map(function(comment) {
+			return (
+				<Comment author = {comment.author}>
+					{comment.text}
+				</Comment>
+			)
+		})
+		return (
+			<div className='commentList'>
+				{commentNodes}
+			</div>
+		)
+	}
+}
+
+class CommentForm extends React.Component {
+	render() {
+		return (
+			<div className = 'commentForm'>
+				I am a commentForm~
+			</div>
+		)
+	}
+}
+
+/* var converter = new Showdown.converter() */
+class Comment extends React.Component {
+	render() {
+		return (
+			<div className = 'comment'>
+				<h2 className = 'commentAuthor'>
+					{this.props.author}
+				</h2>
+				{/* converter.makeHtml(this.props.children.toString()) */}
+				{this.props.children}
+			</div>
+		)
+	}
+}
+
+
+
+export {
+	Hello,
+	CommentBox
 }
--- app/main.js
+++ app/main.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import Hello from './component.jsx';
+import {
+	Hello, CommentBox
+}
+from './component.jsx';
 
 main();
 
 function main() {
-	React.render(<Hello />, document.getElementById('app'));
+	var data = [{
+		author: "Pete Hunt",
+		text: "This is one comment"
+	}, {
+		author: "Jordan Walke",
+		text: "This is *another* comment"
+	}];
+	// React.render(<Hello />, document.getElementById('app'))
+	React.render(<CommentBox data = {data} />, document.getElementById('app'))
 }
